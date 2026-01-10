13e5f487312ad369d0a985b12132117820596fc7
InsideEmpire <garrison.liu@outlook.com>
2026-01-10 14:37:03 -0700

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int percentGrade = int.Parse(grade);
+
+        string letter;
+
+        if (percentGrade >= 90) {
+            letter = "A";
+        }
+        else if (percentGrade >= 80) {
+            letter = "B";
+        }
+        else if (percentGrade >= 70) {
+            letter = "C";
+        }
+        else if (percentGrade >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percentGrade >= 70) {
+            Console.WriteLine("You passed!");
+        }
+        else {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
